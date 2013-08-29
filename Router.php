0b8e0ed1140671c9bb92ebc22488c09aa272539e
Christoffer Winterkvist <christoffer@winterkvist.com>
2013-08-29 09:10:37 +0200

    Began working on Router class

--- Router.php
+++ Router.php
@@ -2,4 +2,20 @@
 
 class Router extends ZENObject {
 	
+	function __construct() {
+		$this->delimiter = '/';
+	}
+	
+    function run($request = '') {
+    	$this->parse_request($request);
+    }
+    
+    function parse_request($string = null) {
+    	$arguments = explode($this->delimiter, $string);
+    	while ($argument = current($arguments)) {
+    	    
+    		next($arguments);
+    	}
+    }
+    
 }
