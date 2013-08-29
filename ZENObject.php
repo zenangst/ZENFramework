80a6e45962f96a3eaecc53d212dc48f27f2509db
Christoffer Winterkvist <christoffer@winterkvist.com>
2013-08-29 09:10:20 +0200

    Added public visibility to all methods in class
    Added __toString description and description method

--- ZENObject.php
+++ ZENObject.php
@@ -4,13 +4,25 @@ class ZENObject {
 	
 	protected $vars = null;
 	
-	function __get($name) {
+	public function __get($name) {
 	    return (isset($this->vars[$name])) ? $this->vars[$name] : null;
 	}
 	
-	function __set($name, $value) {
+	public function __set($name, $value) {
 		$this->vars[$name] = $value;
 	}
+	
+	public function __toString() {
+	    $description = get_class($this);
+	    $description .= print_r(current((array)$this), true);
+	    $description = "<pre>{$description}</pre>";
+	    $description = preg_replace('/array/i', '', $description);
+		return $description;
+	}
+
+    public function description() {
+        return $this;
+    }
 	
 }
 
