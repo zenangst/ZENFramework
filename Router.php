cc0bcb24f9e5b46e70a150ca51bc6ce02a6cd050
Christoffer Winterkvist <christoffer@winterkvist.com>
2013-08-29 09:33:53 +0200

    parse_requests takes current request and populates the class instance with key-value based properties

--- Router.php
+++ Router.php
@@ -12,8 +12,18 @@ class Router extends ZENObject {
     
     function parse_request($string = null) {
     	$arguments = explode($this->delimiter, $string);
+    	$this->arguments = new stdClass();
     	while ($argument = current($arguments)) {
-    	    
+    	    switch (key($arguments)) {
+    	        case 0: $this->controller = $argument; break;
+    	        case 1: $this->method     = $argument; break;
+                default:
+                    if (!isset($this->arguments->$argument)) {
+                        $value = next($arguments);
+                        $this->arguments->$argument = $value;
+                    }
+                    break;
+    	    }
     		next($arguments);
     	}
     }
