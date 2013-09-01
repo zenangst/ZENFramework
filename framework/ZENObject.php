43f79c690e531bc874be92ff62021384cc108aa2
Christoffer Winterkvist <christoffer@winterkvist.com>
2013-09-01 23:28:33 +0200

    Added key path based observers for ZENObject
    You can no set a name for your ZENObjects when using __construct()

--- framework/ZENObject.php
+++ framework/ZENObject.php
@@ -2,6 +2,12 @@
 class ZENObject {
 	
 	protected $vars = null;
+	protected $observers = null;
+	
+	public function __construct($className = null) {
+	    if (!$className)
+		$this->__className = $className;
+	}
 	
 	public function __get($name) {
 	    return (isset($this->vars[$name])) ? $this->vars[$name] : null;
@@ -9,10 +15,15 @@ class ZENObject {
 	
 	public function __set($name, $value) {
 		$this->vars[$name] = $value;
+		if (isset($this->observers[$name]['closure']))
+    		$this->observers[$name]['closure'](
+    		    $this->observers[$name]['keypath'],
+    		    $this->observers[$name]['observer'], $this
+            );
 	}
 	
 	public function __toString() {
-	    $description = get_class($this);
+    	$description = ($this->__className) ? $this->__className : get_class($this);
 	    $description .= print_r(current((array)$this), true);
 	    $description = "<pre>{$description}</pre>";
 	    $description = preg_replace('/array/i', '', $description);
@@ -21,6 +32,30 @@ class ZENObject {
 
     public function description() {
         return $this;
+    }
+    
+    public function addObserver($observer, $keypath, $closure = null) {
+        $key = (is_array($keypath)) ? key($keypath) : $keypath;
+        
+        if (!$closure)
+            $closure = function($keypath, $observer, $target) {
+                $observerKey = current($keypath);
+                $targetKey = key($keypath);
+                $observer->$observerKey = $target->$targetKey;
+            };
+
+        $this->observers[$key] = array(
+            'observer'  => $observer,
+            'closure'  => $closure,
+            'keypath'   => $keypath
+        );
+    }
+    
+    public function removeObserver($key = null) {
+        if ($key)
+        	$this->observers[$key] = null;
+    	else
+    	    unset($this->observers);
     }
 	
 }
