cd3c277b54e21b044079fca44f0cb67ffcb63b95
Christoffer Winterkvist <christoffer@winterkvist.com>
2013-09-02 21:44:05 +0200

    Moved magic methods to top of ZENObject
    Observer key path can no be string

--- framework/ZENObject.php
+++ framework/ZENObject.php
@@ -9,6 +9,21 @@ class ZENObject {
 		$this->__className = $className;
 	}
 	
+	public function __get($name) {
+	    return (isset($this->vars[$name])) ? $this->vars[$name] : null;
+	}
+	
+	public function __set($name, $value) {
+		$this->vars[$name] = $value;
+		// Set observer value
+		if (isset($this->observers[$name]['closure']))
+    		$this->observers[$name]['closure'](
+    		    $this->observers[$name]['keypath'],
+    		    $this->observers[$name]['observer'],
+    		    $this
+            );
+	}
+	
 	public function property($name) {
     	if (property_exists($this, $name))
     	    return $this->$name;
@@ -18,22 +33,10 @@ class ZENObject {
 		$this->$key = $value;
 	}
 	
-	public function __get($name) {
-	    return (isset($this->vars[$name])) ? $this->vars[$name] : null;
-	}
-	
-	public function __set($name, $value) {
-		$this->vars[$name] = $value;
-		if (isset($this->observers[$name]['closure']))
-    		$this->observers[$name]['closure'](
-    		    $this->observers[$name]['keypath'],
-    		    $this->observers[$name]['observer'], $this
-            );
-	}
-	
 	public function __toString() {
     	$description = ($this->__className) ? $this->__className : get_class($this);
-	    $description .= print_r(current((array)$this), true);
+    	$description = 'class ' . $description;
+    	$description .= print_r((array)$this, true);
 	    $description = "<pre>{$description}</pre>";
 	    $description = preg_replace('/array/i', '', $description);
 		return $description;
@@ -46,16 +49,20 @@ class ZENObject {
     public function addObserver($observer, $keypath, $closure = null) {
         $key = (is_array($keypath)) ? key($keypath) : $keypath;
         
-        if (!$closure)
+        if (!$closure) {
             $closure = function($keypath, $observer, $target) {
-                $observerKey = current($keypath);
-                $targetKey = key($keypath);
+                if (is_array($keypath)) {
+                    $observerKey = current($keypath);
+                    $targetKey = key($keypath);
+                } else {
+                    $observerKey = $targetKey = $keypath;
+                }
                 $observer->$observerKey = $target->$targetKey;
             };
-
+        }
         $this->observers[$key] = array(
             'observer'  => $observer,
-            'closure'  => $closure,
+            'closure'   => $closure,
             'keypath'   => $keypath
         );
     }
